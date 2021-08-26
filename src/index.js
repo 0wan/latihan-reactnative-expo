479e706590930460e222812469f6bf126e85eec2
Wawan Setyawan <wawans.setyawan@gmail.com>
2021-08-26 15:01:26 +0700

    Tab Screen Nav

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+import useCachedResources from './hooks/useCachedResources';
 import { Main } from './navigation/Main';
 
 export default function App() {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  }
+
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar style="auto" />
       <NavigationContainer>
         <Main />
       </NavigationContainer>
-    </>
+    </SafeAreaProvider>
   );
 }
