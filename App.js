c80c348b0e90d7a182b54ad4500c72cc37ab293b
Wawan Setyawan <wawans.setyawan@gmail.com>
2021-08-26 13:36:55 +0700

    Branch develop
    Membuat branch develop

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
-import tailwind from 'tailwind-rn';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { SafeAreaView, Text, View } from 'react-native';
+// import tailwind from 'tailwind-rn';
 
-export default function App() {
-  return (
-    <SafeAreaView style={tailwind('h-full')}>
-      <View style={tailwind('pt-12 items-center')}>
-        <View style={tailwind('bg-blue-200 mx-4 p-3 rounded-lg')}>
-          <Text style={tailwind('text-blue-800 font-semibold')}>Open up App.js to start working on your app! Reload Me.</Text>
-          <StatusBar style="auto" />
-        </View>
-      </View>
-    </SafeAreaView>
-    );
-  }
+// export default function App() {
+//   return (
+//     <SafeAreaView style={tailwind('h-full')}>
+//       <View style={tailwind('pt-12 items-center')}>
+//         <View style={tailwind('bg-blue-200 mx-4 p-3 rounded-lg')}>
+//           <Text style={tailwind('text-blue-800 font-semibold')}>Open up App.js to start working on your app! Reload Me.</Text>
+//           <StatusBar style="auto" />
+//         </View>
+//       </View>
+//     </SafeAreaView>
+//     );
+//   }
+
+import App from './src';
+
+export default App;
